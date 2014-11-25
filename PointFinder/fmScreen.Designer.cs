f300b15e14be62798ed3b9d7d22fb17ababe9e10
bokachsub <bokachsub@gmail.com>
2014-11-24 23:16:23 -0800

    Initial CUITGen/PointFinder commit

---
+++ PointFinder/fmScreen.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace PointFinder
+{
+    partial class fmScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fmScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 557);
+            this.Name = "fmScreen";
+            this.Opacity = 0.7D;
+            this.Text = "fmScreen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.fmScreen_DoubleClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ PointFinder/fmScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointFinder
+{
+    public partial class fmScreen : Form
+    {
+        public fmScreen()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //this.Width = Screen.PrimaryScreen.WorkingArea.Width;
+            //this.Height = Screen.PrimaryScreen.WorkingArea.Height;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void fmScreen_DoubleClick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("x: "+Cursor.Position.X.ToString()+" \n"+ "y: "+Cursor.Position.Y.ToString());
+            StreamWriter file = new StreamWriter("GeneratedCode.txt",true);
+            file.WriteLine("Mouse.Click(new Point({0}, {1})); Thread.Sleep(sTime);", Cursor.Position.X, Cursor.Position.Y);
+            file.WriteLine("if ((Control.ModifierKeys & Keys.Shift) != 0) Thread.Sleep(1500000);");
+            file.Close();
+
+        }
+
+
+    }
+}
